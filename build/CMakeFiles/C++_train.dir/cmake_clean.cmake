ce72fab5f046c0b717421cc6a8021ee0ad548f10
Your Name <you@example.com>
2026-01-06 19:32:39 +0900

    CXX学習を開始

---
+++ build/CMakeFiles/C++_train.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "C++_train"
+  "C++_train.pdb"
+  "CMakeFiles/C++_train.dir/src/main.cpp.o"
+  "CMakeFiles/C++_train.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C++_train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!\n";
+
+    cout << sizeof(float) << "\n";
+    cout << sizeof(char) << endl;
+    cout << sizeof(double) << endl;
+    return 0;
+}
