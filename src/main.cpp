8998ceea4c39157acdf5836a426fd3a2f329a753
Your Name <you@example.com>
2026-01-09 19:29:58 +0900

    条件式の書き方を学習

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,29 @@ int main()
     cout << sizeof(char) << "\n";
     cout << sizeof(double) << endl;
     int x;
-    cin >> x;
-    cout << x << "\n";
+    //cin >> x;
+    //cout << x << "\n";
+    int x2 = 10;
+    int y = 2;
+    short s = x2 << y;
+    cout << s;
+    if (x2 <= 5)
+    {
+        cout << "\nx2は5以下です\n";
+    }else
+    {
+        cout << "x2は5以上です\n";
+    }
+    
+    switch (y <= 10)
+    {
+    case 1:
+         cout << "\nx2は10以下です\n";
+        break;
+    
+    default:
+        break;
+    }
+    (y == 2) ? cout << "2\n": cout << "2以外\n";
     return 0;
 }
