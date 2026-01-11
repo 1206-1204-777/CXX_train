b2397514b8305402e742d5fef70316695233fc27
Your Name <you@example.com>
2026-01-11 21:03:49 +0900

    関数に関する学習を開始

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int func(int num){
+    if (num > 100)
+    {
+        return num;
+    }
+    int a = num + 5;
+    
+    return func(a);
+}
+
 int main()
 {
-    cout << "Hello world!\n";
-
-    cout << sizeof(float) << "\n";
-    cout << sizeof(char) << "\n";
-    cout << sizeof(double) << endl;
-    int x;
-    //cin >> x;
-    //cout << x << "\n";
-    int x2 = 10;
-    int y = 2;
-    short s = x2 << y;
-    cout << s;
-    if (x2 <= 5)
-    {
-        cout << "\nx2は5以下です\n";
-    }else
-    {
-        cout << "x2は5以上です\n";
-    }
-    
-    switch (y <= 10)
-    {
-    case 1:
-         cout << "\nx2は10以下です\n";
-        break;
-    
-    default:
-        break;
-    }
-    (y == 2) ? cout << "2\n": cout << "2以外\n";
+    int num;
+    cin >> num;
+    int a = func(num);
+    cout << a << "\n";
     return 0;
 }
