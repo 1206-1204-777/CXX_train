13432ab0bda2fdfb8e867629db0807ab0b42a312
Your Name <you@example.com>
2026-01-18 20:45:01 +0900

    配列操作の方法を学習

--- src/main.cpp
+++ src/main.cpp
@@ -11,5 +11,35 @@ int main()
     int &rA = a;
     rA = 500;
     cout << rA << "\n";
+    int array[4] = {10, 20 , 50};
+    cout << array[2] << "\n";
+
+    // 配列のソート
+    const int num = 5;
+    size_t test[num];
+    for (size_t i = 0; i < num; i++)
+    {
+        cin >> test[i];
+    }
+    for (size_t i = 0; i < num - 1; i++)
+    {
+        for (size_t j = i+1; j < num; j++)
+        {
+            if (test[j] > test[i])
+            {
+                size_t tmp = test[i];
+                test[i] = test[j];
+                test[j] = tmp;
+            }
+            
+        }
+        
+    }
+    for (size_t i = 0; i < num; i++)
+    {
+        cout << test[i] << "\n";
+    }
+    
+    
     return 0;
 }
