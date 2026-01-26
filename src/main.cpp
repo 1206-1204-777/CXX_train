73dbd1cdcce0839f8db635a072e836feece345a7
Your Name <you@example.com>
2026-01-26 23:18:14 +0900

    クラスのカプセル化実験

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 class car
 {
-public:
+private:
 int num;
+public:
 void show();
-
+void setNum(int n);
 };
 
 void car::show(/* args */)
@@ -13,12 +14,24 @@ void car::show(/* args */)
     cout << num << "\n";
 }
 
-
+void car::setNum(int n){
+    if (n <= 0)
+    {
+        cout << "正しい値ではない\n";
+    }
+    
+    num = n;
+}
+void showObject(car c){
+    c.show();
+}
 int main()
 {
     car *car1;
     car1 = new car;
-    car1->num = 460;
+    car1->setNum(0);
     car1->show(); 
+    car1->setNum(45);
+    showObject(*car1);
     return 0;
 }
